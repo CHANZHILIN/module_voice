2f382322946f50852338ca73b04ccef8dd47e830
chanzhilin <1181785848@qq.com>
2019-05-14 16:12:13 +0800

    添加各种Application

---
+++ src/main/java/chen/module_voice/VoiceApplication.java
@@ -0,0 +1,23 @@
+package chen.module_voice;
+
+import android.app.Application;
+
+import chen.baselib.BaseApplication;
+import chen.baselib.IComponentApplication;
+
+/**
+ * Introduce :
+ * Created by CHEN_ on 2019/5/14.
+ * PACKAGE_NAME : chen.module_voice
+ **/
+public class VoiceApplication implements IComponentApplication {
+    @Override
+    public void onCreate(BaseApplication application) {
+
+    }
+
+    @Override
+    public Application getAppliaction() {
+        return null;
+    }
+}
